084ac5bdbfde4b51052e3f7aadf4416cc088fac8
Ethan.0ys <lmt860219@gmail.com>
2023-09-24 06:13:37 +0800

    資料庫存取方式改成：Stored Procedure

--- src/main/java/com/ethan/emall/dao/impl/OrderDaoImpl.java
+++ src/main/java/com/ethan/emall/dao/impl/OrderDaoImpl.java
@@ -6,34 +6,42 @@ import com.ethan.emall.model.OrderDetail;
 import com.ethan.emall.rowmapper.OrderDetailRowMapper;
 import com.ethan.emall.rowmapper.OrderRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class OrderDaoImpl implements OrderDao {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
     public Integer createOrder(Integer memberId, Integer totalAmount) {
-       String sql = "call createOrder(:memberId, :price)";
+//       String sql = "call createOrder(:memberId, :price)";
+        SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate);
 
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("memberId",memberId);
-        map.put("price",totalAmount);
+        HashMap<String, Object> mapIn = new HashMap<>();
+        mapIn.put("in_memberId", memberId);
+        mapIn.put("in_price", totalAmount);
 
-        KeyHolder keyHolder = new GeneratedKeyHolder();
+        SqlParameterSource in = new MapSqlParameterSource(mapIn);
+        Map<String, Object> mapOut = simpleJdbcCall.withProcedureName("createOrder").execute(in);
 
-        namedParameterJdbcTemplate.update(sql,new MapSqlParameterSource(map),keyHolder);
 
-        int orderId = keyHolder.getKey().intValue();
+        int orderId = Integer.parseInt(String.valueOf(mapOut.get("out_orderId")));
 
         return orderId;
     }
@@ -49,15 +57,15 @@ public class OrderDaoImpl implements OrderDao {
             OrderDetail orderDetail = orderDetailList.get(i);
 
             parameterSources[i] = new MapSqlParameterSource();
-            parameterSources[i].addValue("orderId",orderId);
-            parameterSources[i].addValue("productId",orderDetail.getProductId());
-            parameterSources[i].addValue("quantity",orderDetail.getQuantity());
-            parameterSources[i].addValue("standPrice",orderDetail.getStandPrice());
-            parameterSources[i].addValue("itemPrice",orderDetail.getItemPrice());
+            parameterSources[i].addValue("orderId", orderId);
+            parameterSources[i].addValue("productId", orderDetail.getProductId());
+            parameterSources[i].addValue("quantity", orderDetail.getQuantity());
+            parameterSources[i].addValue("standPrice", orderDetail.getStandPrice());
+            parameterSources[i].addValue("itemPrice", orderDetail.getItemPrice());
 
         }
 
-        namedParameterJdbcTemplate.batchUpdate(sql,parameterSources);
+        namedParameterJdbcTemplate.batchUpdate(sql, parameterSources);
     }
 
     @Override
@@ -65,9 +73,9 @@ public class OrderDaoImpl implements OrderDao {
         String sql = "call getOrderById(:orderId)";
 
         HashMap<String, Object> map = new HashMap<>();
-        map.put("orderId",orderId);
+        map.put("orderId", orderId);
 
-        List<Order> orderList = namedParameterJdbcTemplate.query(sql,map,new OrderRowMapper());
+        List<Order> orderList = namedParameterJdbcTemplate.query(sql, map, new OrderRowMapper());
 
         if (orderList.size() > 0) {
             return orderList.get(0);
@@ -81,19 +89,19 @@ public class OrderDaoImpl implements OrderDao {
         String sql = "call getOrderDetailsByOrderId(:orderId)";
 
         HashMap<String, Object> map = new HashMap<>();
-        map.put("orderId",orderId);
+        map.put("orderId", orderId);
 
-        List<OrderDetail> orderDetailList = namedParameterJdbcTemplate.query(sql,map,new OrderDetailRowMapper());
+        List<OrderDetail> orderDetailList = namedParameterJdbcTemplate.query(sql, map, new OrderDetailRowMapper());
 
         return orderDetailList;
     }
 
     @Override
     public List<Order> getOrdersByMember(Integer memberId) {
-        String sql ="call getOrdersByMember(:memberId)";
+        String sql = "call getOrdersByMember(:memberId)";
 
         HashMap<String, Object> map = new HashMap<>();
-        map.put("memberId",memberId);
+        map.put("memberId", memberId);
 
         List<Order> orderList = namedParameterJdbcTemplate.query(sql, map, new OrderRowMapper());
 
